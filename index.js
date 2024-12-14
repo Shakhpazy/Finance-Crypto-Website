3d7584c8718066d8c2a06c04595688e50a5a1b95
yusuf04-s <shakhpazy@spu.edu>
2024-12-13 18:22:52 -0700

    edited the hero pannel styling and functionaly to get inputs in my express project

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 
 const PORT = 3000
 const app = express()
 app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
+    console.log("get")
     res.render("index.ejs")
+})
+
+app.post("/", (req, res) => {
+    console.log("post")
+    console.log(req.body)
 })
 
 app.listen(PORT, () => {
