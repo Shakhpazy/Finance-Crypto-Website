0eda3f134636490ee9e96d798c8c12b92c3bbf71
yusuf04-s <shakhpazy@spu.edu>
2024-12-13 17:32:40 -0700

    file reorg EJS working

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from 'express'
 
+const PORT = 3000
 const app = express()
 app.use(express.static("public"))
-const PORT = 3000
 
 app.get("/", (req, res) => {
-    res.render("")
+    res.render("index.ejs")
 })
 
 app.listen(PORT, () => {
