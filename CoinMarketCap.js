b1e8ecb775069915c4320414d0b7eb9981eff642
yusuf04-s <shakhpazy@spu.edu>
2024-12-17 13:14:28 -0800

    the table displays with all information now, Next fix should be responsive design for the table

--- CoinMarketCap.js
+++ CoinMarketCap.js
@@ -28,15 +28,67 @@ export class coinmarketcap {
 
         try {
               const response = await axios.get(url, { headers, params });
-              const data = response.data;
-        
+              const data = response.data.data;
+              let stringSymbol = "";
               // Example: Log the first cryptocurrency
-              //console.log(data.data[0]);
               //return all data in key values
-              return data.data
+              console.log("API IS USED");
+              console.log(data);
+              (data).forEach(crypto => {
+                stringSymbol += crypto.symbol + ",";
+              });
+              console.log(stringSymbol);
+              const logoLinks = await this.getCoinLogo(stringSymbol);
+              console.log(logoLinks);
+
+              for (const key in logoLinks) {
+                const link = logoLinks[key]
+                for (let i = 0; i < data.length; i++) {
+                    const crypto = data[i];
+                    if (crypto.id == key) {
+                        crypto["logo"] = link;
+                        break; // Break out of the inner loop
+                    }
+                }
+              }
+              return data
           } catch (error) {
-            console.log("failed api")
+            console.log("API FAILED")
+            console.log(error)
           }
+    }
+
+    //param StringofSymbols = "BTC,ETH,XRP"
+    async getCoinLogo(StringofSymbols) {
+        const url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/info"
+
+        const headers = {
+            "Accept": "application/json",
+            "X-CMC_PRO_API_KEY": API,
+        }
+
+        const params = {
+            "symbol" : StringofSymbols
+        }
+
+        try {
+            const response = await axios.get(url, { headers, params })
+            const data = response.data.data
+            let logosLink = {}
+            for (const key in data) {
+                logosLink[data[key].id] = data[key].logo
+            }
+
+            return logosLink
+        } catch (error) {
+            console.log("API FAILED")
+            console.log(error)
+        }
+    }
+
+    async fetchCoin() {
+        //returns information given a coin like 
+        // market cap and previous history of the coin
     }
 
 
