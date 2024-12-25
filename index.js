f94295d28f77274b41584cc49ffaa82eac51f169
yusuf04-s <shakhpazy@spu.edu>
2024-12-24 22:59:00 -0800

    started fixing ejs hieight for chart

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get("/coin", async (req, res) => {
     //let theData = {name: 'Bitcoin', symbol: 'BTC', circulating_supply: 19796721, price: 104763.486, volume_24h: 62172072377.837494, max_supply: 21000000}
     const theData = (await API.fetchCryptoData(20))
     //res.render("index.ejs", {data : theData})
-    res.render("coin.ejs", {data : theData, crypto1 : undefined, crypto2 : undefined})
+    res.render("coin.ejs")
 })
 
 app.listen(PORT, () => {
